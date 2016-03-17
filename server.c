6edaea7b46502e6cfba75a9574628e62a82232dc
Bfgeshka <bfg@derpy.ru>
2016-03-17 18:07:15 +0900

    server.c update

--- server.c
+++ server.c
@@ -1,12 +1,12 @@
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netdb.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #include "server.h"
 

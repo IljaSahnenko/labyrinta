92ea28603cc32e6420f503f3d4343886248ff470
IljaSahnenko <daf232@mail.ru>
2016-03-25 00:17:35 +1000

    Added files via upload

--- client.cpp
+++ client.cpp
@@ -1,17 +1,37 @@
+/*
+ * test.cxx
+ *
+ * Copyright 2016 nod <nod@nod-Lenovo-G580>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ *
+ */
+
+
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_rect.h>
 #include <SDL2/SDL_render.h>
 #include <SDL2/SDL_rwops.h>
-#include <SDL2/SDL_video.h>
 #include <iomanip>
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <xcb/render.h>
-
-#include <unistd.h>
-
 
 SDL_DisplayMode displayMode;
 
@@ -55,20 +75,18 @@ void render_UPDATE(SDL_Renderer* render, SDL_Texture* texture[], SDL_Rect* destr
 
 int main()
 {
-
-	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
-	{
-		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
-		return 1;
-	}
+	
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
+	std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+	return 1;
+}
 
 
 	SDL_Window *win = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
-	if (win == NULL)
-	{
-		std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
-		return 1;
-	}
+if (win == NULL){
+	std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+	return 1;
+}
 	SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 	if (ren == NULL)
 	{
@@ -95,61 +113,60 @@ int main()
 		std::cout << "SDL_LoadBMP Error: " << SDL_GetError() << std::endl;
 		return 1;
 	}
-	SDL_Delay(20000);
+
 	SDL_Texture *background = SDL_CreateTextureFromSurface(ren, BMP_background);
 	SDL_FreeSurface(BMP_background); //Очищение памяти поверхности
-
+	
 	SDL_Texture *player =  IMG_LoadTexture(ren, "res/kosmos-81.jpg");
 	if (player == NULL)
 	{
 		std::cout << "SDL_CreateTextureFromSurface Error: " << SDL_GetError() << std::endl;
 		return 1;
-		/*
-			}
-						SDL_Event event;
-						bool quit = false;
-						while(!quit)
-							while(SDL_PollEvent(&event))
-							{
-								SDL_PumpEvents(); // обработчик событий.
-							}
 
-		*/
-		SDL_RenderClear(ren); //Очистка рендера
-		SDL_RenderCopy(ren, background, NULL, &background_RECT); //Копируем в рендер фон
-		SDL_RenderCopy(ren, player, NULL, &player_RECT); //Копируем в рендер персонажа
-		SDL_RenderPresent(ren); //Погнали!!
-		/*
-			if((keyboardState[SDL_SCANCODE_UP])||(keyboardState[SDL_SCANCODE_W]))
-				move_UP(ren,player,player_RECT);
-
-			if((keyboardState[SDL_SCANCODE_DOWN])||(keyboardState[SDL_SCANCODE_S]))
-				move_DOWN(ren,player,player_RECT);
-
-			if((keyboardState[SDL_SCANCODE_LEFT])||(keyboardState[SDL_SCANCODE_A]))
-				move_LEFT(ren,player,player_RECT);
-
-			if((keyboardState[SDL_SCANCODE_RIGHT])||(keyboardState[SDL_SCANCODE_D]))
-				move_RIGHT(ren,player,player_RECT);
-
-
-		//ZOOM----------------------------------------------------------------
-			if(keyboardState[SDL_SCANCODE_KP_PLUS])
-			{
-				TESTtexture_SCALE += 0.02;
-				player_RECT.h = player_HEIGH * TESTtexture_SCALE;
-				player_RECT.w = player_WIGHT * TESTtexture_SCALE;
-			}
-			if(keyboardState[SDL_SCANCODE_KP_MINUS])
-			{
-				TESTtexture_SCALE -= 0.02;
-				player_RECT.h = player_HEIGH * TESTtexture_SCALE;
-				player_RECT.w = player_WIGHT * TESTtexture_SCALE;
-			}
-		*/
 	}
-	SDL_RenderPresent(ren);
+				SDL_Event event;
+				bool quit = false;
+				while(!quit)
+					while(SDL_PollEvent(&event))
+					{
+						SDL_PumpEvents(); // обработчик событий.
+					}
 
+
+	SDL_RenderClear(ren); //Очистка рендера
+	SDL_RenderCopy(ren, background, NULL, &background_RECT); //Копируем в рендер фон
+	SDL_RenderCopy(ren, player, NULL, &player_RECT); //Копируем в рендер персонажа
+	SDL_RenderPresent(ren); //Погнали!!
+
+	if((keyboardState[SDL_SCANCODE_UP])||(keyboardState[SDL_SCANCODE_W]))
+		move_UP(ren,player,player_RECT);
+
+	if((keyboardState[SDL_SCANCODE_DOWN])||(keyboardState[SDL_SCANCODE_S]))
+		move_DOWN(ren,player,player_RECT);
+
+	if((keyboardState[SDL_SCANCODE_LEFT])||(keyboardState[SDL_SCANCODE_A]))
+		move_LEFT(ren,player,player_RECT);
+
+	if((keyboardState[SDL_SCANCODE_RIGHT])||(keyboardState[SDL_SCANCODE_D]))
+		move_RIGHT(ren,player,player_RECT);
+
+
+//ZOOM----------------------------------------------------------------
+	if(keyboardState[SDL_SCANCODE_KP_PLUS])
+	{
+		TESTtexture_SCALE += 0.02;
+		player_RECT.h = player_HEIGH * TESTtexture_SCALE;
+		player_RECT.w = player_WIGHT * TESTtexture_SCALE;
+	}
+	if(keyboardState[SDL_SCANCODE_KP_MINUS])
+	{
+		TESTtexture_SCALE -= 0.02;
+		player_RECT.h = player_HEIGH * TESTtexture_SCALE;
+		player_RECT.w = player_WIGHT * TESTtexture_SCALE;
+	}
+
+	SDL_RenderPresent(ren);	
+	SDL_Delay(20000);
 	SDL_DestroyTexture(player);
 	SDL_DestroyTexture(background);
 
@@ -158,4 +175,5 @@ int main()
 	SDL_Quit();
 	return 0;
 }
-// g++ client.cpp -O2 -Wall -I/usr/include/SDL2 -D_REENTRANT -L/usr/lib/x86_64-linux-gnu -lSDL2 -lSDL2_image -o client
+	
+// g++ client.cpp -O2 -Wall -I/usr/include/SDL2 -D_REENTRANT -L/usr/lib/x86_64-linux-gnu -lSDL2 -lSDL2_image -o client 
